1fb7244b8c462fd36ee92580f28c41c6a2871472
Timothy Cope <kirbycope@gmail.com>
2025-02-17 19:42:27 -0800

    button guide

--- scenes/main.gd
+++ scenes/main.gd
@@ -7,15 +7,28 @@ const TITLE_THEME = preload("res://assets/title_theme.ogg")
 
 @onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var audio_player_3d: AudioStreamPlayer3D = $MarioHead/AudioStreamPlayer3D
+@onready var animation_player = $MarioHead/AnimationPlayer
+@onready var background = $Background
+@onready var buttons: Control = $Camera3D/CanvasLayer/Buttons
+@onready var cursor = $Camera3D/CanvasLayer/Cursor
 @onready var logo = $Logo/logo
 @onready var logo_1996 = $"1996"
 @onready var logo_tm = $TM
-@onready var background = $Background
-@onready var cursor = $Camera3D/CanvasLayer/Cursor
+@onready var mario_head = $MarioHead
 @onready var press_start = $Camera3D/CanvasLayer/PressStart
 @onready var transition = $Camera3D/CanvasLayer/Transition
-@onready var mario_head = $MarioHead
-@onready var animation_player = $MarioHead/AnimationPlayer
+@onready var touch_controls: RichTextLabel = $Camera3D/CanvasLayer/TouchControls
+
+
+func _input(event: InputEvent) -> void:
+	if press_start.visible:
+		if event is InputEventScreenDrag or event is InputEventScreenTouch:
+			buttons.show()
+			touch_controls.hide()
+		else:
+			buttons.hide()
+			touch_controls.show()
+
 
 func _ready():
 	logo.visible = true
@@ -27,6 +40,7 @@ func _ready():
 	press_start.visible = false
 	mario_head.visible = false
 	start_sequence()
+
 
 func start_sequence():
 	audio_player.stream = COIN
@@ -69,3 +83,47 @@ func start_sequence():
 		audio_player_3d.stream = HELLO
 		audio_player_3d.play()
 	)
+
+
+func _on_button_b_button_down() -> void:
+	mario_head.switch_zoom_level()
+
+
+func _on_button_c_down_button_down() -> void:
+	Input.action_press("rotate_down")
+
+
+func _on_button_c_down_button_up() -> void:
+	Input.action_release("rotate_down")
+
+
+func _on_button_c_left_button_down() -> void:
+	Input.action_press("rotate_left")
+
+
+func _on_button_c_left_button_up() -> void:
+	Input.action_release("rotate_left")
+
+
+func _on_button_c_right_button_down() -> void:
+	Input.action_press("rotate_right")
+
+
+func _on_button_c_right_button_up() -> void:
+	Input.action_release("rotate_right")
+
+
+func _on_button_c_up_button_down() -> void:
+	Input.action_press("rotate_up")
+
+
+func _on_button_c_up_button_up() -> void:
+	Input.action_release("rotate_up")
+
+
+func _on_button_r_button_down() -> void:
+	Input.action_press("hold")
+
+
+func _on_button_r_button_up() -> void:
+	Input.action_release("hold")
--- scenes/mario_head.gd
+++ scenes/mario_head.gd
@@ -7,22 +7,17 @@ var mesh_arrays = []
 var original_vertices = []
 var original_materials = []
 var is_dragging = false
-var grabbed_vertices = []  # Store indices of all grabbed vertices
+var grabbed_vertices = []
 var closest_surface_idx = -1
 var deform_radius = 0.3
-var grab_radius = 0.1     # Radius for initial vertex selection
+var grab_radius = 0.1
 var drag_start_pos = Vector3.ZERO
 var drag_plane_normal = Vector3.ZERO
 var zoom_level: int = 0
 
-func _ready() -> void:
-	for surface_idx in range(mesh_instance.mesh.get_surface_count()):
-		var surface_arrays = mesh_instance.mesh.surface_get_arrays(surface_idx)
-		mesh_arrays.append(surface_arrays)
-		original_vertices.append(surface_arrays[Mesh.ARRAY_VERTEX].duplicate())
-		original_materials.append(mesh_instance.mesh.surface_get_material(surface_idx))
 
 func _input(event: InputEvent) -> void:
+
 	if event.is_action_pressed("ui_cancel"):
 		reset_mesh()
 		return
@@ -35,13 +30,12 @@ func _input(event: InputEvent) -> void:
 		if event.pressed:
 			var from = camera.project_ray_origin(event.position)
 			var to = from + camera.project_ray_normal(event.position) * 100
-			
+
 			# Find closest point first
 			var min_dist = INF
 			var closest_point = Vector3.ZERO
 			closest_surface_idx = -1
 			grabbed_vertices.clear()
-			
 			for surface_idx in range(mesh_arrays.size()):
 				var vertices = mesh_arrays[surface_idx][Mesh.ARRAY_VERTEX]
 				for i in range(vertices.size()):
@@ -54,6 +48,7 @@ func _input(event: InputEvent) -> void:
 						closest_surface_idx = surface_idx
 
 			if min_dist < 0.5:
+
 				# Now grab all vertices within grab_radius of the closest point
 				var vertices = mesh_arrays[closest_surface_idx][Mesh.ARRAY_VERTEX]
 				for i in range(vertices.size()):
@@ -62,11 +57,12 @@ func _input(event: InputEvent) -> void:
 					var dist = global_vertex.distance_to(closest_point)
 					if dist < grab_radius:
 						grabbed_vertices.append(i)
-				
+
 				if grabbed_vertices.size() > 0:
 					is_dragging = true
 					drag_start_pos = closest_point
 					drag_plane_normal = (camera.global_position - closest_point).normalized()
+
 		else:
 			is_dragging = false
 			if not Input.is_action_pressed("hold"):
@@ -106,13 +102,8 @@ func _input(event: InputEvent) -> void:
 		drag_start_pos = current_pos
 
 	if event.is_action_pressed("zoom"):
-		print(zoom_level)
-		if zoom_level < 2:
-			zoom_level += 1
-			position.z -= 0.2
-		else:
-			zoom_level = 0
-			position.z += 0.4
+		switch_zoom_level()
+
 
 func _process(delta: float) -> void:
 	if not animation_player.is_playing():
@@ -126,6 +117,23 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("rotate_up"):
 		rotation.x -= delta * 2
 
+
+func _ready() -> void:
+	for surface_idx in range(mesh_instance.mesh.get_surface_count()):
+		var surface_arrays = mesh_instance.mesh.surface_get_arrays(surface_idx)
+		mesh_arrays.append(surface_arrays)
+		original_vertices.append(surface_arrays[Mesh.ARRAY_VERTEX].duplicate())
+		original_materials.append(mesh_instance.mesh.surface_get_material(surface_idx))
+
+
+func get_distance_to_ray(point: Vector3, ray_origin: Vector3, ray_end: Vector3) -> float:
+	var ray_direction = (ray_end - ray_origin).normalized()
+	var v = point - ray_origin
+	var t = v.dot(ray_direction)
+	var p = ray_origin + t * ray_direction
+	return point.distance_to(p)
+
+
 func reset_mesh() -> void:
 	for surface_idx in range(mesh_arrays.size()):
 		var surface_arrays = mesh_arrays[surface_idx]
@@ -136,6 +144,7 @@ func reset_mesh() -> void:
 	is_dragging = false
 	grabbed_vertices.clear()
 
+
 func update_mesh() -> void:
 	var new_mesh = ArrayMesh.new()
 	for idx in range(mesh_arrays.size()):
@@ -143,9 +152,11 @@ func update_mesh() -> void:
 		new_mesh.surface_set_material(idx, original_materials[idx])
 	mesh_instance.mesh = new_mesh
 
-func get_distance_to_ray(point: Vector3, ray_origin: Vector3, ray_end: Vector3) -> float:
-	var ray_direction = (ray_end - ray_origin).normalized()
-	var v = point - ray_origin
-	var t = v.dot(ray_direction)
-	var p = ray_origin + t * ray_direction
-	return point.distance_to(p)
+
+func switch_zoom_level() -> void:
+	if zoom_level < 2:
+		zoom_level += 1
+		position.z -= 0.2
+	else:
+		zoom_level = 0
+		position.z += 0.4
